7a16af0dedf8d1b6e631754a63662ad5acfbf3a1
Muhammad Owais <rahneverd@gmail.com>
2024-05-09 10:10:23 +0300

    express template added

--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
-console.log('Hello world')
+import express from 'express';
+
+const app = express();
+
+let PORT = 3000
+
+app.get('/', (req, res) => {
+	res.send('Hello world');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
